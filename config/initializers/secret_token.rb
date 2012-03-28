b11c0c98426658761749ccfb827a8daf399c0a1f
Ryan Yeske <rcyeske@gmail.com>
2012-03-28 16:57:21 -0700

    initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Duluth::Application.config.secret_token = '14bb3a48f06bfb35a7ad0f1171f82b37f3abdd20de7c70a284078c152036ab9d31aa81f0459499d3cb82b8640a7aafaf97bf036dd85838a526827252b7a75eb7'
