6b41e62e7d07865e4876f20663fd26d9ef2a0981
Ryan Yeske <rcyeske@gmail.com>
2012-04-24 17:56:10 -0700

    add token authentication

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120413224527) do
+ActiveRecord::Schema.define(:version => 20120423054740) do
 
   create_table "items", :force => true do |t|
     t.string   "summary"
@@ -54,8 +54,10 @@ ActiveRecord::Schema.define(:version => 20120413224527) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
+    t.string   "authentication_token"
   end
 
+  add_index "users", ["authentication_token"], :name => "index_users_on_authentication_token", :unique => true
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
 
